befe49770483d8b2f9296cb08878f5bc1f6bf269
iVampireSP.com <im@ivampiresp.com>
2023-11-08 17:10:12 +0800

    增加 delete 方法

--- GinMiddleware_AllowAllCors.go
+++ GinMiddleware_AllowAllCors.go
@@ -9,7 +9,7 @@ func (GinMiddlewareStruct) AllowAllCors() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Jwt-Payload")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
