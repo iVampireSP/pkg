d380e79154587cd9f6583c803a2b187c9c273131
iVampireSP.com <im@ivampiresp.com>
2023-11-21 19:34:17 +0800

    移除 接口，增加 分页组件

---
+++ pagination.go
@@ -0,0 +1,57 @@
+package pkg
+
+import (
+	"gorm.io/gorm"
+)
+
+type Paginator struct {
+	DB        *gorm.DB
+	Page      int
+	Limit     int
+	Sort      string
+	Total     int64
+	Resources interface{}
+}
+
+type PaginationParams struct {
+	Page  int
+	Limit int
+	Sort  string
+}
+
+func (UtilsStruct) NewPaginator(db *gorm.DB, resources interface{}, params PaginationParams) *Paginator {
+	return &Paginator{
+		DB:        db,
+		Page:      params.Page,
+		Limit:     params.Limit,
+		Sort:      params.Sort,
+		Resources: resources,
+	}
+}
+
+func (p *Paginator) Paginate() error {
+	offset := (p.Page - 1) * p.Limit
+
+	query := p.DB.Model(p.Resources)
+	if p.Sort != "" {
+		query = query.Order(p.Sort)
+	}
+
+	if err := query.Count(&p.Total).Error; err != nil {
+		return err
+	}
+
+	if err := query.Limit(p.Limit).Offset(offset).Find(p.Resources).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Paginator) GetResult() interface{} {
+	return p.Resources
+}
+
+func (p *Paginator) GetTotalCount() int64 {
+	return p.Total
+}
--- pkg.go
+++ pkg.go
@@ -1,20 +1,7 @@
 package pkg
 
-import "github.com/gin-gonic/gin"
-
-type utilsInterface interface {
-	PasswordHash(pwd string) (string, error)
-	PasswordVerify(pwd string, hash string) bool
-	GetAuth(c *gin.Context) (*JwtStruct, bool)
-}
 type UtilsStruct struct{}
-
-var Utils utilsInterface = UtilsStruct{}
-
-type GinMiddlewareInterface interface {
-	JwtUser() gin.HandlerFunc
-	AllowAllCors() gin.HandlerFunc
-}
 type GinMiddlewareStruct struct{}
 
-var GinMiddleware GinMiddlewareInterface = GinMiddlewareStruct{}
+var Utils = UtilsStruct{}
+var GinMiddleware = GinMiddlewareStruct{}
